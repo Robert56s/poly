52235016210ae5427d73f2d1c4daa3be58779cd0
Robert56s <78372422+Robert56s@users.noreply.github.com>
2025-10-02 14:31:33 -0400

    bhjsfjbhsd

--- vs/Bob/TD3/exo1/exo1.cpp
+++ vs/Bob/TD3/exo1/exo1.cpp
@@ -1,9 +1,35 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
-bool isPrime(int number) {
+static bool isPrime(int number) {
 
+	if (number <= 1) return false;
+
+	int i = 2;
+	while (i * i <= number) {
+		if (number % i == 0) {
+			return false;
+		}
+		i++;
+	}
+	return true;
+}
+
+static int getSmallestIntDivisor(int number) {
+	int divisor = 2;
+	if (number % divisor == 0) return divisor;
+	
+	divisor++;
+
+	while (divisor <= sqrt(number)) {
+		if (number % divisor == 0) {
+			return divisor;
+		}
+		divisor += 2;
+	}
+	return number;
 }
 
 int main() {
@@ -11,6 +37,11 @@ int main() {
 
 	cout << "Entrer un nombre entier : ";
 	cin >> number;
+
+	if (isPrime(number)) {
+		cout << "Ce nombre est premier.";
+	}
+	cout << "Ce nombre n'est pas premier car il est divisible par " << getSmallestIntDivisor(number) << ".";
 
 
 }
