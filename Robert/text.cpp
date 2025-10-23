191d6220618b46a2201299ee1e795eab9bab68eb
Robert56s <78372422+Robert56s@users.noreply.github.com>
2025-10-22 21:39:20 -0400

    TD4 et autre

---
+++ Robert/text.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+int numero = 0;
+string rue = "";
+
+cout << "Rentez une phrase : ";
+cin >> numero;
+
+getline(cin, rue)
+
+cout << numero << " " << rue;
